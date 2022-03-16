e4b5347c6f033f766b4657482b8af0a91e4a27c7
Dario Campomori <killingdario@gmail.com>
2022-03-16 12:38:28 +0100

    Menu version 2

---
+++ TutorialMenu/menu.h
@@ -0,0 +1,72 @@
+#include <curses.h>
+#include <string>
+
+using namespace std;
+
+#ifndef _MENU_H_
+#define _MENU_H_
+
+class Menu
+{
+public:
+  int start_x;
+  string text;
+  char trigger;
+
+  Menu(string text, char trigger)
+  {
+    this->text = text;
+    this->trigger = trigger;
+  }
+};
+
+class MenuBar
+{
+public:
+  WINDOW *win;
+  Menu *menus;
+  int num_menus;
+  int current_position;
+  int selected_menu;
+
+  MenuBar(WINDOW *win, Menu *menus, int num_menus)
+  {
+    this->win = win;
+    this->menus = menus;
+    this->num_menus = num_menus;
+    this->current_position = 2;
+    this->selected_menu = -1;
+
+    for (int i = 0; i < num_menus; i++)
+    {
+      this->menus[i].start_x = current_position;
+      current_position += this->menus[i].text.length() + 1;
+    }
+  }
+
+  void draw()
+  {
+    for (int i = 0; i < num_menus; i++)
+    {
+      if (selected_menu == i)
+      {
+        wattron(win, A_STANDOUT);
+      }
+      mvwprintw(win, 0, this->menus[i].start_x, this->menus[i].text.c_str());
+      wattroff(win, A_STANDOUT);
+    }
+  }
+
+  void handleTrigger(char trigger)
+  {
+    for (int i = 0; i < num_menus; i++)
+    {
+      if (trigger = this->menus[i].trigger)
+      {
+        selected_menu = i;
+      }
+    }
+  }
+};
+
+#endif
--- TutorialMenu/tutorialMenu.cpp
+++ TutorialMenu/tutorialMenu.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <curses.h>
+#include "menu.h"
 
 int main(int argc, char **argv)
 {
@@ -15,44 +16,78 @@ int main(int argc, char **argv)
   WINDOW *win = newwin(yMax / 2, xMax / 2, yMax / 4, xMax / 4);
   box(win, 0, 0);
 
-  mvwprintw(win, 0, 2, "File");
-  mvwprintw(win, 0, 7, "Edit");
-  mvwprintw(win, 0, 12, "Options");
+  Menu menus[3] = {
+      Menu("File", 'f'),
+      Menu("Edit", 'e'),
+      Menu("Options", 'o'),
+  };
+
+  MenuBar menubar = MenuBar(win, menus, 3);
+  menubar.draw();
 
   char ch;
   while (ch = wgetch(win))
   {
-    switch (ch)
-    {
-    case 'f':
-      wattron(win, A_STANDOUT);
-      mvwprintw(win, 0, 2, "File");
-      wattroff(win, A_STANDOUT);
-      mvwprintw(win, 0, 7, "Edit");
-      mvwprintw(win, 0, 12, "Options");
-      break;
-    case 'e':
-      wattron(win, A_STANDOUT);
-      mvwprintw(win, 0, 7, "Edit");
-      wattroff(win, A_STANDOUT);
-      mvwprintw(win, 0, 2, "File");
-      mvwprintw(win, 0, 12, "Options");
-      break;
-    case 'o':
-      wattron(win, A_STANDOUT);
-      mvwprintw(win, 0, 12, "Options");
-      wattroff(win, A_STANDOUT);
-      mvwprintw(win, 0, 2, "File");
-      mvwprintw(win, 0, 7, "Edit");
-      break;
-    default:
-      mvwprintw(win, 0, 2, "File");
-      mvwprintw(win, 0, 7, "Edit");
-      mvwprintw(win, 0, 12, "Options");
-      break;
-    }
+    menubar.handleTrigger(ch);
+    menubar.draw();
   }
 
   endwin();
   return 0;
 }
+
+// VERSION 1
+// int main(int argc, char **argv)
+// {
+
+//   initscr();
+//   noecho();
+//   curs_set(0);
+
+//   int yMax, xMax;
+//   getmaxyx(stdscr, yMax, xMax);
+
+//   WINDOW *win = newwin(yMax / 2, xMax / 2, yMax / 4, xMax / 4);
+//   box(win, 0, 0);
+
+//   mvwprintw(win, 0, 2, "File");
+//   mvwprintw(win, 0, 7, "Edit");
+//   mvwprintw(win, 0, 12, "Options");
+
+//   char ch;
+//   while (ch = wgetch(win))
+//   {
+//     switch (ch)
+//     {
+//     case 'f':
+//       wattron(win, A_STANDOUT);
+//       mvwprintw(win, 0, 2, "File");
+//       wattroff(win, A_STANDOUT);
+//       mvwprintw(win, 0, 7, "Edit");
+//       mvwprintw(win, 0, 12, "Options");
+//       break;
+//     case 'e':
+//       wattron(win, A_STANDOUT);
+//       mvwprintw(win, 0, 7, "Edit");
+//       wattroff(win, A_STANDOUT);
+//       mvwprintw(win, 0, 2, "File");
+//       mvwprintw(win, 0, 12, "Options");
+//       break;
+//     case 'o':
+//       wattron(win, A_STANDOUT);
+//       mvwprintw(win, 0, 12, "Options");
+//       wattroff(win, A_STANDOUT);
+//       mvwprintw(win, 0, 2, "File");
+//       mvwprintw(win, 0, 7, "Edit");
+//       break;
+//     default:
+//       mvwprintw(win, 0, 2, "File");
+//       mvwprintw(win, 0, 7, "Edit");
+//       mvwprintw(win, 0, 12, "Options");
+//       break;
+//     }
+//   }
+
+//   endwin();
+//   return 0;
+// }
