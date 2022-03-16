886d995f10e681fa412fda17916a6e6218a1f501
Dario Campomori <killingdario@gmail.com>
2022-03-16 14:02:17 +0100

    Menu version 3

--- TutorialMenu/tutorialMenu.cpp
+++ TutorialMenu/tutorialMenu.cpp
@@ -10,16 +10,29 @@ int main(int argc, char **argv)
   noecho();
   curs_set(0);
 
+  if (!has_colors())
+  {
+    return -1;
+  }
+
+  start_color();
+
+  init_pair(1, COLOR_WHITE, COLOR_BLUE);
+
   int yMax, xMax;
   getmaxyx(stdscr, yMax, xMax);
 
   WINDOW *win = newwin(yMax / 2, xMax / 2, yMax / 4, xMax / 4);
   box(win, 0, 0);
 
+  string menu1[] = {"New", "Open", "Save", "Exit"};
+  string menu2[] = {"Copy", "Cut", "Paste"};
+  string menu3[] = {"Sidebar", "Terminal"};
+
   Menu menus[3] = {
-      Menu("File", 'f'),
-      Menu("Edit", 'e'),
-      Menu("Options", 'o'),
+      Menu("File", 'f', menu1, 4),
+      Menu("Edit", 'e', menu2, 3),
+      Menu("View", 'v', menu3, 2),
   };
 
   MenuBar menubar = MenuBar(win, menus, 3);
